13183248b2dd6a1fc536ac39de3ad2b2aa727ec0
harshvardh6n <66harshvardhan99@gmail.com>
2023-12-24 20:31:32 +0530

    question 1

--- problem-solving/q1.c
+++ problem-solving/q1.c
@@ -1,1 +1,12 @@
 // WAP to convert given second into its equivalent hour, minute and second as per the following format.
+#include<stdio.h>
+int main(void){
+    int sec;
+    printf("Enter no of seconds :");
+    scanf("%d",&sec);
+    int hour,minute,secs;
+    hour=sec/3600;
+    minute=(sec%3600)/60;
+    secs=(sec%3600)%60;
+    printf("%d seconds equals %d hours %d minutes and %d seconds.",sec,hour,minute,secs);
+}
